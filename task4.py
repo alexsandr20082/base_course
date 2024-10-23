6d26120919ba1c529d734bf2e7342e8fc177a722
alexsandr20082 <yxo1981@gmail.com>
2024-10-23 08:34:17 +0000

    1 работа

---
+++ task4.py
@@ -0,0 +1,12 @@
+a = [1,5, "Good", "Bad"]
+b = [9, "Blue","Red", 11]
+number1 = a[1] + b[3]
+print(number1)
+number2 = a[2] + b[2]
+print(number2)
+number3 = a[0] * b[0]
+print(number3)
+number4 = a[1] ** b[3]
+print(number4)
+number5 = a + b 
+print(number5)
