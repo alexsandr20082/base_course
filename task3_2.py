82c1216c58ae830258508b845363cdd59f990dbf
alexsandr20082 <yxo1981@gmail.com>
2024-10-23 14:17:08 +0000

    3 работа

--- task3_2.py
+++ task3_2.py
@@ -7,7 +7,7 @@ v = np.sqrt((g * h * np.tan(b)**2) / (2 * np.cos(a) * (1 - np.tan(b) * np.tan(a)
 print("Скорость v:",v,"м/с")
 
 T = 200
-k = 1,380649e-23
+k = 1.380649e-23
 E = 300
 N = ((2 / np.sqrt(np.pi)) * (h / (k * T)**(3/2)) * E ** (-E / (k * T)) * E ** (T/2))
 print ("N:",N)
