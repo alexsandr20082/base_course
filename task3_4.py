c8e6d1a4469e09637fdf315f13e28a64687e9440
alexsandr20082 <yxo1981@gmail.com>
2024-11-18 15:50:02 +0000

    3 работа

--- task3_4.py
+++ task3_4.py
@@ -2,12 +2,12 @@ import numpy as np
 N = int(input("Введите число N (число строк): "))
 M = int(input("Введите число M (число столбцов): "))
 
-trigonometry = np.zeros((N, M))
+trigonometry_array = np.zeros((N, M))
 
 for i in range(N):
     for j in range(M):
         value = np.sin(N * i + M * j + 1)
-        trigonometry[i, j] = value if value >= 0 else 0
+        trigonometry_array[i, j] = value if value >= 0 else 0
 
 print("Конечный массив:")
-print(trigonometry)
+print(trigonometry_array)
--- task3_5.py
+++ task3_5.py
@@ -1,15 +1,18 @@
 import numpy as np
-N = 5
-M = 15
-trigonometry_array = np.zeros((N,M))
-for i in range(N):
-    for j in range(M):
-        reply = np.sin(i * M * j)
-        trigonometry_array[i][j] = max(reply,0)
-print("Исходный массив: ")
+N = int(input("Введите число N (число строк): "))
+M = int(input("Введите число M (число столбцов): "))
+
+trigonometry_array = np.zeros((N, M))
+print("Исходный массив перед заменой столбцов:")
 print(trigonometry_array)
-pop1,pop2 = 0,1
-trigonometry_array[:, [pop1,pop2]] = trigonometry_array[:, [pop1,pop2]]
-print("Массив после замены столбцов: ")
+
+col1 = int(input("Введите индекс первого столбца для замены (от 0 до {}): ".format(M - 1)))
+col2 = int(input("Введите индекс второго столбца для замены (от 0 до {}): ".format(M - 1)))
+
+if 0 <= col1 < M and 0 <= col2 < M:
+    trigonometry_array[:, [col1, col2]] = trigonometry_array[:, [col2, col1]]
+else:
+    print("Некорректные индексы столбцов.")
+
+print("Массив после замены столбцов:")
 print(trigonometry_array)
- 
