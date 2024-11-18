e99a4609d691bb04480fcff1a728b4b01257cb27
alexsandr20082 <yxo1981@gmail.com>
2024-11-18 15:43:36 +0000

    3 работа

--- task3_4.py
+++ task3_4.py
@@ -1,9 +1,13 @@
 import numpy as np
-N = 5
-M = 15
-trigonometry_array = np.zeros((N,M))
+N = int(input("Введите число N (число строк): "))
+M = int(input("Введите число M (число столбцов): "))
+
+trigonometry = np.zeros((N, M))
+
 for i in range(N):
     for j in range(M):
-        reply = np.sin(i * M * j)
-        trigonometry_array[i][j] = max(reply,0)
-print(trigonometry_array)
+        value = np.sin(N * i + M * j + 1)
+        trigonometry[i, j] = value if value >= 0 else 0
+
+print("Конечный массив:")
+print(trigonometry)
