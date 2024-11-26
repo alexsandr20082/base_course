6af4b0498acecd55688c5a4f8c087e1e0e325731
alexsandr20082 <yxo1981@gmail.com>
2024-11-26 16:45:30 +0000

    6 работа

--- task6_3.py
+++ task6_3.py
@@ -1,7 +1,20 @@
-import matplotlib.pyplot as plt
 import numpy as np
-x = int(input("Введите значение x "))
-y = int(input("Введите значение y"))
-N = int(input("Введите количество точек N "))
-def circle_photter():
-    S = (x**2/a**2) + (y**2/b**2) = 1
+import matplotlib.pyplot as plt
+
+def plot_ellipse(x_min, x_max, N):
+    a = 5 
+    b = 3
+    x = np.linspace(x_min, x_max, N)
+    y_positive = b * np.sqrt(1 - (x**2) / (a**2))
+    y_negative = -b * np.sqrt(1 - (x**2) / (a**2)) 
+    
+    plt.plot(x, y_positive, color='black')
+    plt.plot(x, y_negative, color='black')
+    plt.title('График эллипса')
+    plt.axhline(0, color='black',linewidth=0.5)
+    plt.axvline(0, color='black',linewidth=0.5)
+    plt.legend()
+    plt.savefig('fig_7.png')
+    plt.axis('equal')
+
+plot_ellipse(-6, 6, 1000)
