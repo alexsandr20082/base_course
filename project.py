49f06591a85d2ab49f962f467350a2d838bb08e4
alexsandr20082 <yxo1981@gmail.com>
2025-01-21 18:58:52 +0000

    проект

--- project.py
+++ project.py
@@ -1,26 +1,69 @@
-from matplotlib.animation import FuncAnimation
-import matplotlib.pyplot as plt
 import numpy as np
- 
-def circle_move(R, vx0,vy0,time) :
-    x0 = vx0 
-    y0 = vy0 * time
-    alpha = np.arange(0,2*np.pi,0.1)
-    x = x0 + R*np.cos(alpha)
-    y = y0 + R*np.sin(alpha)
-    return x,y
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+num_steps = 10
+step_height = 1
+step_width = 2
+move_right_speed = 0.025
+bounce_factor = 1
+
+
+#Ступеньки
+x_steps = np.arange(num_steps) * step_width
+y_steps = np.arange(num_steps) * step_height
+
+
+ball_radius = 0.2
+ball_x = step_width / 2 
+ball_y = num_steps * step_height + ball_radius 
+velocity_y = 0  
+gravity = -0.01  
 
 fig, ax = plt.subplots()
-ball, = plt.plot([], [], 'o', color='r')
- 
-def animate(i):
-    ball.set_data(circle_move(R=0.01, vx0=0.01, vy0=0.01, time=i))
-    return ball
+ax.set_xlim(-1, num_steps * step_width + 1)
+ax.set_ylim(0, num_steps * step_height + 3)
+
+#Лестница
+for i in range(num_steps):
+    step = plt.Rectangle((x_steps[i], y_steps[i]), step_width, step_height, color='brown')
+    ax.add_artist(step)
+
+#Мяч
+ball = plt.Circle((ball_x, ball_y), ball_radius, color='red')
+ax.add_artist(ball)
+
+
+def update(frame):
+    global ball_y, velocity_y, ball_x
+
+    ball_x += move_right_speed
+
     
-edge = 2
-plt.axis('equal')
-ax.set_xlim(-edge, edge)
-ax.set_ylim(edge, -edge)
- 
-ani = FuncAnimation(fig, animate, frames=200, interval=30)
-ani.save('animation_3.gif', writer="pillow")
+    velocity_y  += gravity 
+    ball_y += velocity_y 
+
+    #Столкновение
+    current_step = int(ball_x // step_width) 
+    if current_step < num_steps and ball_y - ball_radius <= y_steps[current_step]:
+        ball_y = y_steps[current_step] + ball_radius 
+        velocity_y = -velocity_y * bounce_factor  
+
+
+    if current_step == num_steps - 1 and velocity_y < 0:
+        velocity_y = 0 
+
+   
+    ball.set_center((ball_x, ball_y))
+    return ball,
+
+
+ani = animation.FuncAnimation(fig, update, frames=300, interval=20)
+
+plt.title("Анимация мяча, падающего на лестницу")
+plt.xlabel("Ширина")
+plt.ylabel("Высота")
+plt.grid()
+plt.gca().set_aspect('equal', adjustable='box') 
+plt.show()
+ani.save('animation_7.gif')
