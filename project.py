f2c75b78c109a0caa3d3def5d729afaafeccd152
alexsandr20082 <yxo1981@gmail.com>
2025-01-21 17:37:27 +0000

    7 работа

---
+++ project.py
@@ -0,0 +1,35 @@
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+def circle_move(R, vx0,vy0,time) :
+    x0 = vx0 
+    y0 = vy0 * time
+    alpha = np.arange(0,2*np.pi,0.1)
+    x = x0 + R*np.cos(alpha)
+    y = y0 + R*np.sin(alpha)
+    return x,y
+
+fig, ax = plt.subplots()
+ball, = plt.plot([], [], 'o', color='r')
+ 
+def animate(i):
+    ball.set_data(circle_move(R=0.01, vx0=0.01, vy0=0.01, time=i))
+    return ball
+    
+edge = 2
+plt.axis('equal')
+ax.set_xlim(-edge, edge)
+ax.set_ylim(edge, -edge)
+ 
+ani = FuncAnimation(fig, animate, frames=200, interval=30)
+ani.save('animation_3.gif', writer="pillow")
+import matplotlib.pyplot as plt
+x = [1,1,5,5,1]
+y = [1,5,5,1,1]
+plt.plot(x,y,color='black',label='Graf 1', marker='o',ms=5)
+plt.axis('equal')
+plt.savefig('animation_3.gif')
+
+
+
