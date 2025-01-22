a5043dd95ce6172b41dd76012e3f26e043789533
alexsandr20082 <yxo1981@gmail.com>
2025-01-22 14:15:15 +0000

    проект

--- project.py
+++ project.py
@@ -61,8 +61,6 @@ def update(frame):
 ani = animation.FuncAnimation(fig, update, frames=300, interval=20)
 
 plt.title("Анимация мяча, падающего на лестницу")
-plt.xlabel("Ширина")
-plt.ylabel("Высота")
 plt.grid()
 plt.gca().set_aspect('equal', adjustable='box') 
 plt.show()
